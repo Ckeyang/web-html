cf46e085ad69b6af5a680c847000317b50f04d8e
ckey <ckey@transing.cn>
2017-02-20 18:01:50 +0800

    提交登录demo，新建template js

---
+++ src/js/modules/cktemplate/cktemplate.js
@@ -0,0 +1,10 @@
+/**
+ * Created by intel on 2017/2/20.
+ */
+define('cktemplate', function () {
+    var tempalte = function (id, data) {
+        var that = this;
+
+    };
+    return template;
+});
