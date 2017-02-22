3c19243f0dc65324b389e6b0c858bfce40861a29
Ckeyang <463650967@qq.com>
2017-02-22 21:44:15 +0800

    编写几种datepicker的demo

--- src/js/view/api/main.js
+++ src/js/view/api/main.js
@@ -21,4 +21,27 @@ require(['jquery', 'ck','foundation-datepicker'], function ($,ck) {
     $('#timepicker').fdatepicker({
         format: 'yyyy-mm-dd hh:ii'
     });
+    $('#timepicker2').fdatepicker();
+    var nowTemp = new Date();
+    var now = new Date(nowTemp.getFullYear(), nowTemp.getMonth(), nowTemp.getDate(), 0, 0, 0, 0);
+    var checkin = $('#timepicker3').fdatepicker({
+        onRender: function (date) {
+            return date.valueOf() < now.valueOf() ? 'disabled' : '';
+        }
+    }).on('changeDate', function (ev) {
+        if (ev.date.valueOf() > checkout.date.valueOf()) {
+            var newDate = new Date(ev.date)
+            newDate.setDate(newDate.getDate() + 1);
+            checkout.update(newDate);
+        }
+        checkin.hide();
+        $('#dpd2')[0].focus();
+    }).data('datepicker');
+    var checkout = $('#timepicker4').fdatepicker({
+        onRender: function (date) {
+            return date.valueOf() <= checkin.date.valueOf() ? 'disabled' : '';
+        }
+    }).on('changeDate', function (ev) {
+        checkout.hide();
+    }).data('datepicker');
 });
